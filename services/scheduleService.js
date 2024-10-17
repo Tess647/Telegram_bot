11d5899d61938265d1235c86578e31cf1a7ba0f7
Tess647 <thanni_taslihah@yahoo.com>
2024-10-17 19:41:24 +0100

    debugging scheduleService.js

--- services/scheduleService.js
+++ services/scheduleService.js
@@ -4,11 +4,16 @@ const { sendRandomMessage } = require('./messageService');
 
 function scheduleDailyMessage(chatId, time) {
     const [hour, minute] = time.split(':');
+    console.log(`Scheduling daily message for user ${chatId} at ${time}`);
     cron.schedule(`${minute} ${hour} * * *`, async () => {
         const user = await User.findOne({ userId: chatId });
+        console.log(`Fetched user for scheduled message: ${user}`);
         if (user && user.dailyTime) {
             const randomCategory = Object.keys(messages)[Math.floor(Math.random() * 3)];
+            console.log(`Sending random message from category: ${randomCategory}`);
             sendRandomMessage(chatId, randomCategory);
+        } else {
+            console.log(`No user found or dailyTime not set for user ${chatId}`);
         }
     });
 }
